ce6a2deda1dad18e17af6ffddd8c00c7e88409b7
likachedia <86976784+likachedia@users.noreply.github.com>
2024-02-05 23:11:24 +0400

    feat: sort list

--- src/files/list.js
+++ src/files/list.js
@@ -1,13 +1,41 @@
 import fs from "node:fs/promises";
 
+function compareByType( a, b ) {
+  if ( a.type < b.type && a.name < b.name){
+    return -1;
+  }
+  if ( a.type > b.type && a.name > b.name){
+    return 1;
+  }
+  return 0;
+}
+
+function compare( a, b ) {
+  if ( a.name < b.name ){
+    return -1;
+  }
+  if ( a.name > b.name ){
+    return 1;
+  }
+  return 0;
+}
+
 const list = async () => {
-    const files = await fs.readdir(process.cwd());
-    const data = files.map( async (file) => {
-      const stat = await fs.lstat(file);
-      const type = stat.isFile() ? 'file' : stat.isDirectory() ? 'directory' : 'other';
-      return {name: file, type: type};
+    const files = await fs.readdir(process.cwd(), {withFileTypes: true});
+    const dirArr = [];
+    const fileArr = [];
+    files.forEach(async file => {
+      file.isDirectory() ? dirArr.push(file) : fileArr.push(file);
     })
-    Promise.all(data).then(data => {
+
+    const data1 = dirArr.map( async (file) => {
+      return {name: file.name, type: 'directory'};
+    })
+    const data2 = fileArr.map( async (file) => {
+      return {name: file.name, type: 'file'};
+    })
+    const sorted = [...data1.sort(compare),...data2.sort(compare)];
+    Promise.all(sorted).then(data => {
       console.table(data);
       console.log(`You are currently in ${process.cwd()}`);
     } ).catch(err => console.error('Operation failed'));
